6aee3a3bc077793c8e5c80bf38b14f3527629397
Edwin Körner <edwin.korner@medieinstitutet.se>
2022-03-14 14:43:36 +0100

    Har fixat layout och animationer för uppgiftsformuläret

---
+++ src/components/form.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "../CSS/form.css"
+
+function form() {
+  return (
+    <div className="mainDiv">
+        <div className="formDiv">
+            <span>Ange kontaktuppgifter för att slutföra beställningen</span>
+            <div className='inputDiv'>
+            
+            <input name='Förnamn' id="textInput" type="text" required minLength={1} />
+            <label id="inputLabel" htmlFor="Förnamn">Förnamn</label>
+
+            <input name='Efternamn' id="textInput" type="text" required minLength={1} />
+            <label id="inputLabel" htmlFor="Efternamn">Efternamn</label>
+
+            <input name='Mail' id="textInput" type="text" required minLength={1} />
+            <label id="inputLabel" htmlFor="Mail">Mail</label>
+
+            <input name='Telefonnummer' id="textInput" type="text" required minLength={1} />
+            <label id="inputLabel" htmlFor="Telefonnummer">Telefonnummer</label>
+
+            <input name='Adress' id="textInput" type="text" required minLength={1} />
+            <label id="inputLabel" htmlFor="Adress">Adress</label>
+
+            </div>
+            <div className="buttonDiv">
+                <button className='buyButton'>Slutför beställning</button>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default form
