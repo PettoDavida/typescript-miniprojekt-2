d9ea86f7ce00cb2ffa97b4828cc183beffcb219c
PettoDavida <maximilian.widman@gmail.com>
2022-03-14 23:28:33 +0100

    added classnames to products

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -5,7 +5,7 @@ function ProductCard(){
         
     return(
         <div>
-            {clothes.map((_object: any, _i: number) => {return (<div><img src={clothes[_i].image} alt="" /> <h3>{clothes[_i].name}</h3></div>)})}
+            {clothes.map((_object: any, _i: number) => {return (<div className="productDiv"><img className="productImg" src={clothes[_i].image} alt="" /> <h3 className="productName">{clothes[_i].name}</h3></div>)})}
         </div>
     )
 
