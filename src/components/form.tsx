c8662755fd50d78b98a38bce6e6d51b5f97f48f3
Edwin Körner <edwin.korner@medieinstitutet.se>
2022-03-16 13:21:22 +0100

    Uppgiftsformuläret klart

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -8,19 +8,19 @@ function form() {
             <span>Ange kontaktuppgifter för att slutföra beställningen</span>
             <div className='inputDiv'>
             
-            <input name='Förnamn' id="textInput" type="text" required minLength={1} />
+            <input name='Förnamn' id="textInput" type="text" required minLength={1} autoComplete="given-name" />
             <label id="inputLabel" htmlFor="Förnamn">Förnamn</label>
 
-            <input name='Efternamn' id="textInput" type="text" required minLength={1} />
+            <input name='Efternamn' id="textInput" type="text" required minLength={1} autoComplete="family-name" />
             <label id="inputLabel" htmlFor="Efternamn">Efternamn</label>
 
-            <input name='Mail' id="textInput" type="text" required minLength={1} />
+            <input name='Mail' id="textInput" type="text" required minLength={1} autoComplete="email" />
             <label id="inputLabel" htmlFor="Mail">Mail</label>
 
-            <input name='Telefonnummer' id="textInput" type="text" required minLength={1} />
+            <input name='Telefonnummer' id="textInput" type="text" required minLength={1} autoComplete="tel" />
             <label id="inputLabel" htmlFor="Telefonnummer">Telefonnummer</label>
 
-            <input name='Adress' id="textInput" type="text" required minLength={1} />
+            <input name='Adress' id="textInput" type="text" required minLength={1} autoComplete="street-address" />
             <label id="inputLabel" htmlFor="Adress">Adress</label>
 
             </div>
