4eb5a452d23cdbb25515dc8aed6076ddfff2f6e0
Edwin Körner <edwin.korner@medieinstitutet.se>
2022-03-16 16:41:36 +0100

    Fixat fraktsätt sidan. Designen behöver fixas lite

---
+++ src/components/fraktsätt.tsx
@@ -0,0 +1,118 @@
+import React, {useState} from 'react'
+import "../CSS/fraktsätt.css"
+
+function Fraktsätt() {
+
+    let formDiv = document.getElementsByClassName("formDiv");
+
+    const[value, setValue] = useState(0);
+    let postnordExtended = "inactive"
+    let postnordHemExtended = "inactive"
+    let DHLExtended = "inactive"
+    let instaboxExtended = "inactive"
+
+    if(value === 1){
+        postnordExtended = "postnordExtended"
+      } else if (value === 2){
+        postnordHemExtended = "postnordHemExtended"
+      } else if (value === 3){
+        DHLExtended = "DHLExtended"
+      } else if (value === 4){
+        instaboxExtended = "instaboxExtended"
+      }
+
+      console.log(value)
+  return (
+    <div className="mainDiv">
+        <div className="formDiv">
+            <span>Välj fraktsätt</span>            
+
+            <div className="postnordDiv">
+            <input name='Radio' id="postnord" type="radio" onClick={() => setValue(1)}/>
+            <label id="inputLabel" htmlFor="Postnord">Postnord Ombud</label>
+            <div className={postnordExtended}>
+            <div className='leveransDag'>
+            <p>Fri frakt!</p>
+            <br />
+            <p>Förväntad leveransdag</p>
+            <strong>Lördagen den 26:e mars</strong>
+            </div>
+            <div className='leveransPlats'>
+            <p>Levereras till</p>
+            <strong>Okq8 Bärnstensgatan</strong>
+            <p>Bärnstensgatan 3</p>
+            <p>426 52 Västra frölunda</p>
+            </div>
+            </div>
+            </div>
+            
+            <div className="postnordHemDiv">
+            <input name='Radio' id="postnordHem" type="radio" onClick={() => setValue(2)} />
+            <label id="inputLabel" htmlFor="PostnordHem">Postnord hemleverans</label>
+            <div className={postnordHemExtended}>
+            <div className='leveransDag'>
+            <p>29 kr</p>
+            <br />
+            <p>Förväntad leveransdag</p>
+            <strong>Lördagen den 26:e mars (kl. 10-17) </strong>
+            </div>
+            <div className='leveransPlats'>
+            <p>Levereras till</p>
+            <strong>Din adress</strong>
+            </div>
+
+            </div>
+            </div>
+
+            <div className="DHLDiv">
+            <input name='Radio' id="DHL" type="radio"  onClick={() => setValue(3)} />
+            <label id="inputLabel" htmlFor="DHL">DHL</label>
+            <div className={DHLExtended}>
+            <div className='leveransDag'>
+            <p>Fri frakt!</p>
+            <br />
+            <p>Förväntad leveransdag</p>
+            <strong>Onsdagen den 23:e mars</strong>
+            </div>
+            <div className='leveransPlats'>
+            <p>Levereras till</p>
+            <strong>Ica nära Fiskebäck</strong>
+            <p>Mungårdsgatan 21</p>
+            <p>426 53 Västra frölunda</p>
+            </div>
+
+            </div>
+            </div>
+
+            <div className="instaboxDiv">
+            <input name='Radio' id="instabox" type="radio" onClick={() => setValue(4)} />
+            <label id="inputLabel" htmlFor="Instabox">Instabox</label>
+            <div className={instaboxExtended}>
+            <div className='leveransDag'>
+            <p>Fri frakt!</p>
+            <br />
+            <p>Förväntad leveransdag</p>
+            <strong>Måndagen den 21:e mars</strong>
+            </div>
+            <div className='leveransPlats'>
+            <p>Levereras till</p>
+            <strong>Okq8 Bärnstensgatan</strong>
+            <p>Bärnstensgatan 3</p>
+            <p>42652 Västra frölunda</p>
+            </div>
+
+            </div>
+            </div>
+
+            <div className="buttonDiv">
+                <button className='buyButton'>Slutför beställning</button>
+                </div>
+            </div>
+
+            </div>
+  )
+  
+}
+
+
+export default Fraktsätt
