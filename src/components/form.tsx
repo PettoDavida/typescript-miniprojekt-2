68d35f25c161271161e5e6081fef5cfb88c4163d
Edwin Körner <edwin.korner@medieinstitutet.se>
2022-03-17 12:43:33 +0100

    Fixat så att telefonnummret sparas i variabeln "input"

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../CSS/form.css"
 
-function form() {
+function Form() {
+  
+  const [input, setInput] = useState('');
   return (
     <div className="mainDiv">
         <div className="formDiv">
             <span>Ange kontaktuppgifter för att slutföra beställningen</span>
             <div className='inputDiv'>
             
-            <input name='Förnamn' id="textInput" type="text" required minLength={1} autoComplete="given-name" />
+            <input name='Förnamn' className="textInput" type="text" required minLength={1} autoComplete="given-name" />
             <label id="inputLabel" htmlFor="Förnamn">Förnamn</label>
 
-            <input name='Efternamn' id="textInput" type="text" required minLength={1} autoComplete="family-name" />
+            <input name='Efternamn' className="textInput" type="text" required minLength={1} autoComplete="family-name" />
             <label id="inputLabel" htmlFor="Efternamn">Efternamn</label>
 
-            <input name='Mail' id="textInput" type="text" required minLength={1} autoComplete="email" />
+            <input name='Mail' className="textInput" type="text" required minLength={1} autoComplete="email" />
             <label id="inputLabel" htmlFor="Mail">Mail</label>
 
-            <input name='Telefonnummer' id="textInput" type="text" required minLength={1} autoComplete="tel" />
+            <input name='Telefonnummer' className="textInput" value={input} onInput={e => setInput(e.currentTarget.value)} type="text" required minLength={1} autoComplete="tel" />
             <label id="inputLabel" htmlFor="Telefonnummer">Telefonnummer</label>
 
-            <input name='Adress' id="textInput" type="text" required minLength={1} autoComplete="street-address" />
+            <input name='Adress' className="textInput" type="text" required minLength={1} autoComplete="street-address" />
             <label id="inputLabel" htmlFor="Adress">Adress</label>
 
             </div>
@@ -32,4 +34,4 @@ function form() {
   )
 }
 
-export default form
+export default Form
