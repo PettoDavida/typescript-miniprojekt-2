80706e3c2868da8dbf2813ae613f629660d1c844
AdrianRydin <adrian.rydin@gmail.com>
2022-03-30 15:47:27 +0200

    Fixade payments

--- src/components/payment.tsx
+++ src/components/payment.tsx
@@ -1,27 +1,63 @@
+import { useState } from "react";
 import "../CSS/payment.css";
+import { Button } from "@mui/material/Button";
+import { Stack } from "@mui/material";
 
 function PaymentMethod() {
+  const [value, setValue] = useState(0);
+
+  let payment = "payment";
+  let paymentText = document.getElementById("paymentText");
+  let SwishDiv = document.getElementById("SwishDiv");
+  let SwishQR = document.getElementById("SwishQR");
+
+  if (value === 1) {
+    payment = "hidden";
+    paymentText!.innerHTML = "Swish";
+    SwishDiv!.style.display = "flex";
+  } else if (value === 2) {
+    payment = "hidden";
+  } else if (value === 3) {
+    payment = "hidden";
+  }
+
   return (
     <div className="payment-div">
       <div className="choose-payment">
-        <h2>Välj betalningsätt!</h2>
+        <h2 id="paymentText">Välj betalningsätt!</h2>
       </div>
       <div className="payment-methods">
-        <div className="payment">
+        <div className={payment} id="payment" onClick={() => setValue(1)}>
           <p>Swish</p>
 
           <img src="images/Swish.png" className="swish-logo" alt="" />
         </div>
-        <div className="payment">
+        <div className={payment} id="payment" onClick={() => setValue(2)}>
           <p>Betalkort</p>
 
           <img src="images/Mastercard.png" className="betalkort-logo" alt="" />
         </div>
-        <div className="payment">
+        <div className={payment} id="payment" onClick={() => setValue(3)}>
           <p>Paypal</p>
 
           <img src="images/Paypal.png" className="paypal-logo" alt="" />
         </div>
+      </div>
+
+      <div className="swish-payment" id="SwishDiv">
+        <img
+          src="images/SwishQR.png"
+          alt=""
+          className="swish-qr"
+          id="SwishQR"
+        />
+        <p>Scanna QR koden eller skriv in telefonnumret i rutan under</p>
+        <input type="text" name="phone" id="TextboxSwish" />
+        <Stack direction="row" spacing={2}>
+          <Button>Betala</Button>
+        </Stack>
+        {/*         <input type="button" value="Betala" id="paymentButtonSwish" />
+         */}{" "}
       </div>
     </div>
   );
